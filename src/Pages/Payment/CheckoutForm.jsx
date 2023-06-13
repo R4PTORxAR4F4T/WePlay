973fced45aeaed705b2c145904320aacaed2a712
R4PTORxAR4F4T <h.oppy2671@gmail.com>
2023-06-13 06:16:42 +0600

    pay amount complete client secret error

--- src/Pages/Payment/CheckoutForm.jsx
+++ src/Pages/Payment/CheckoutForm.jsx
@@ -1,10 +1,30 @@
-import {useState} from 'react';
+import {useEffect, useState} from 'react';
 import { CardElement, useElements, useStripe } from '@stripe/react-stripe-js';
+import useAuth from '../../hooks/useAuth';
 
-const CheckoutForm = () => {
+const CheckoutForm = ({cart,price}) => {
+
+    console.log(price)
+    const {user} = useAuth;
     const stripe = useStripe();
     const elements = useElements();
     const [cardError, setCardError] = useState('');
+    const [clientSecret, setClientSecret] = useState('');
+    const [processing, setProcessing] = useState(false);
+    const [transactionId, setTransactionId] = useState('');
+
+    useEffect(()=>{
+        fetch("http://localhost:5000/create-payment-intent",{
+            method: 'POST', 
+            headers: {
+                'content-type': 'application/json'
+            }, 
+            body: JSON.stringify(price)
+        })
+        .then((res)=>{
+            setClientSecret(res.data.clientSecret);
+        })
+    },[price])
 
     const handleSubmit = async(event)=>{
         event.preventDefault()
@@ -26,11 +46,57 @@ const CheckoutForm = () => {
 
         if (error) {
             console.log('[error]', error);
-            setCardError(error.message);
+            setCardError(error.message.clientSecret);
         }
         else {
             setCardError('');
-            console.log('[PaymentMethod]', paymentMethod);
+            // console.log('[PaymentMethod]', paymentMethod);
+        }
+
+        setProcessing(true)
+
+        const { paymentIntent, error: confirmError } = await stripe.confirmCardPayment(
+            clientSecret,
+            {
+                payment_method: {
+                    card: card,
+                    billing_details: {
+                        email: user?.email || 'unknown',
+                        name: user?.displayName || 'anonymous'
+                    },
+                },
+            },
+        );
+
+        if (confirmError) {
+            setCardError(confirmError);
+        }
+
+        // console.log(paymentIntent);
+        setProcessing(false)
+        if (paymentIntent.status === 'succeeded') {
+            setTransactionId(paymentIntent.id);
+            // save payment information to the server
+            const payment = {
+                email: user?.email,
+                transactionId: paymentIntent.id,
+                price,
+                date: new Date(),
+                status: 'service pending',
+            }
+                fetch('http://localhost:5000/payments',{
+                    method: 'POST', 
+                    headers: {
+                        'content-type': 'application/json'
+                    }, 
+                    body: JSON.stringify(payment)
+                })
+                .then(res => {
+                    console.log(res.data);
+                    if (res.data.result.insertedId) {
+                        console.log("success")
+                    }
+                })
         }
     }
 
@@ -53,7 +119,7 @@ const CheckoutForm = () => {
                 },
                 }}
             />
-            <button className="btn btn-outline btn-sm" type="submit" disabled={!stripe}>
+            <button className="btn btn-outline btn-sm" type="submit" disabled={!stripe || !clientSecret || processing}>
                 Pay
             </button>
             </form>
--- src/Pages/Payment/Payment.jsx
+++ src/Pages/Payment/Payment.jsx
@@ -1,18 +1,34 @@
 import { loadStripe } from "@stripe/stripe-js";
 import { Elements } from '@stripe/react-stripe-js';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import CheckoutForm from './CheckoutForm';
+import useAuth from "../../hooks/useAuth";
 
 const stripePromise = loadStripe(import.meta.env.VITE_Payment_Gateway_PK);
 
 const Payment = () => {
+
+    const {user, loading} = useAuth();
+    const [cart, setCart] = useState();
+
+    const url = `http://localhost:5000/carts?email=${user?.email}`;
+
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setCart(data))
+
+    }, [url,loading]);
+
+    const total = cart ? cart.reduce((sum, cartitem) => sum + parseFloat(cartitem.price), 0) : 0;
+
     return (
         <div className="w-5/6">
             <div>
             <p className='text-4xl text-center border-b pb-4 border-white text-orange-300'>Payment</p>
         </div>
         <Elements stripe={stripePromise}>
-            <CheckoutForm></CheckoutForm>
+            <CheckoutForm cart={cart} price={total}></CheckoutForm>
         </Elements>
         </div>
     );
